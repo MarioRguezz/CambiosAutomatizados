b4634b9a36403bb9c87de6fb56d3eb04d2e55e37
jossrodes <js.rzer@gmail.com>
2017-10-09 16:39:23 +0000

    server changes

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ if [[ `sudo git status -uno` ]]; then
   sudo git commit -am "server changes"
   sudo git pull origin master
   sudo git push origin master
+  sudo git config user.name "jossrodes"
 else
   echo "no changes available"
 fi
