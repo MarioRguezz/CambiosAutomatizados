71d9a20c74fecd108a2b2a7507a0066e1998e82e
jossrodes <js.rzer@gmail.com>
2017-10-09 16:28:51 +0000

    server changes

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
-if [[ `git status -uno` ]]; then
-  git add .
-  git commit -am "server changes"
-  git pull origin master
-  git push origin master
+if [[ `sudo git status -uno` ]]; then
+  sudo git add .
+  sudo git commit -am "server changes"
+  sudo git pull origin master
+  sudo git push origin master
 else
   echo "no changes available"
 fi
