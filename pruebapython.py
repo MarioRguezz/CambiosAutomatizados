422af29ad7baea391f9a55a2bd55b63254f7169c
Mario Negrete Rodríguez <rasmus_darkmariozp@hotmail.com>
2017-10-09 10:23:03 -0500

    cambios

--- pruebapython.py
+++ pruebapython.py
@@ -1,13 +1,13 @@
 if __name__ == '__main__':
-    num = int(input()) 
-    n = num % 2 
-    if n == 0 and (2<= num <=5): 
+    num =  int(raw_input("What is your number? "))
+    n = num % 2
+    if n == 0 and (2<= num <=5):
          print('Not Weird')
-    elif n == 0 and (6<= num <=20): 
+    elif n == 0 and (6<= num <=20):
           print('Weird')
-    elif n == 0 and num > 20: 
+    elif n == 0 and num > 20:
           print("Not Weird")
-    elif num % 2 != 0: 
+    elif num % 2 != 0:
           print('Weird')
-    elif n != 0: 
+    elif n != 0:
           print('Weird')
