0477c213d8c22ab03f6b6c4daa68929d90477192
jossrodes <js.rzer@gmail.com>
2017-10-09 16:47:51 +0000

    server changes

--- test.py
+++ test.py
@@ -1,9 +1,8 @@
-if [[ `sudo git status -uno` ]]; then
+if [[ `sudo git status --porcelain` ]]; then
   sudo git add .
   sudo git commit -am "server changes"
   sudo git pull origin master
   sudo git push origin master
-  sudo git config user.name "jossrodes"
 else
   echo "no changes available"
 fi
